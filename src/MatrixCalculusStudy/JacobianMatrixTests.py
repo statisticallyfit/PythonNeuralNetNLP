c0f6d796c5c9f1772063025d3e99c1f7ac39680a
BabitzdaBrave <amg.vintila@gmail.com>
2019-06-27 04:28:29 -0400

    save, trying to star file

--- src/MatrixCalculusStudy/JacobianMatrixTests.py
+++ src/MatrixCalculusStudy/JacobianMatrixTests.py
@@ -2,63 +2,60 @@ import numpy as np
 import xarray as xr
 import numdifftools as nd
 import matplotlib.pyplot as plt
-#from typing import  *
 
 
-def main():
-    # example data array
-    theArray = xr.DataArray([11,22,33], dims=['x'], coords={'x': [0,1,2]})
-    print(theArray)
-    print("\n")
-    print("first: " + theArray.x0)
-    print("second: " + theArray.x1)
-    print("third: " + theArray.x2)
-    #testJacobianMatrixOfScalarFunctionIsGradient()
-    # exp_jacobian_03()
-
-
-def rosen(x: xr.DataArray) -> np.double :
-    return (1 - x.x0) ** 2 + 105. * (x[1] - x[0] ** 2) ** 2
-
-    #return (1 - x[0]) ** 2 + 105. * (x[1] - x[0] ** 2) ** 2
-#def rosen(x : np.ndarray) -> np.double:
-#    return (1 - x[0]) ** 2 + 105. * (x[1] - x[0] ** 2) ** 2
-
-
-def exp_jacobian_01():
-    grad = nd.Gradient(rosen)([1, 1])
-    assert np.allclose(grad, 0) == True
-
+# example data array
+#theArray = xr.DataArray([11,22,33], dims=['x'], coords={'x': [0,1,2]})
+#    print(theArray)
+#    print("\n")
+#    print("first: " + theArray.x0)
+#    print("second: " + theArray.x1)
+#    print("third: " + theArray.x2)
 
 X1T = np.float64
 X2T = np.float64
 X3T = np.float64
 
 
+
+
+def main():
+    testJacobianMatrixOfScalarFunctionIsFunctionGradient()
+    # exp_jacobian_03()
+
+
+# The rosenbrock function
+def multivarScalarFunction(x: np.array) -> np.double :
+    return (1 - x[0]) ** 2 + 105. * (x[1] - x[0] ** 2) ** 2
+
+
+
+
 #
 # `Jacobian matrix` of a `scalar function` is the `function gradient`
 #
-#def exp_jacobian_02():
-def testJacobianMatrixOfScalarFunctionIsGradient():
+def testJacobianMatrixOfScalarFunctionIsFunctionGradient():
 
-    x1s = np.linspace(start=10.0, stop=20.0, num=333, dtype=X1T)
-    x2s = np.linspace(start=20.0, stop=45.0, num=333, dtype=X2T)
-    x3s = np.linspace(start=30.0, stop=98.0, num=333, dtype=X2T)
-    xs = np.array(list(zip(x1s, x2s, x3s)))
-        # np.array(
-        # np.array(list(zip(x1s, x2s, x3s)))
-        # dtype=np.dtype([('x1', X1T), ('x2', X2T), ('x3', X3T)]))
+    x1s = np.linspace(start=10.0, stop=20.0, num=333, dtype=X1T)  #array
+    x2s = np.linspace(start=20.0, stop=45.0, num=333, dtype=X2T) # array
+    x3s = np.linspace(start=30.0, stop=98.0, num=333, dtype=X2T) # array
+    xs = np.array(list(zip(x1s, x2s, x3s))) # matrix
+        #np.array([x1s, x2s, x3s]) #
 
-    jb = nd.Jacobian(rosen)
+    jb = nd.Jacobian(multivarScalarFunction)
 
     # Apply the Jacobian function to all 3 dimensional points in the linear space
+    # There are 2 variables in the function and so the jacobian has the form:
+    # ( df1 / dx1   df1 / dx2 ) ????????????????????????????????????????????????????????
+    # ------ ( df2 / dx1   df2 / dx2 )
+    # ------ ( df2 / dx1   df2 / dx2 )
     for x in xs:
         x1 = x[0]
         x2 = x[1]
         x3 = x[2]
 
         jac = jb([x1, x2, x3])
-        grad = nd.Gradient(rosen)([x1, x2, x3])
+        grad = nd.Gradient(multivarScalarFunction)([x1, x2, x3])
         assert np.allclose(jac, grad) == True
 
 
