5ef117cb95877e1d0173f05dac18e561cc3fcae3
BabitzdaBrave <amg.vintila@gmail.com>
2019-10-30 10:09:14 -0400

    save

--- src/NLPstudy/AnnotatedTransformer.py
+++ src/NLPstudy/AnnotatedTransformer.py
@@ -343,4 +343,35 @@ class Embeddings(nn.Module):
         return self.lut(x) * math.sqrt(self.d_model)
 
 
-# something else
+
+
+
+###
+# Positional Encodings
+###
+
+# Since our model contains no recurrence and no convolution, in order for the model to make use of
+# the order of the sequence, we must inject some information about the relative or absolute position
+# of the tokens in the sequence. To this end, we add “positional encodings” to the input embeddings
+# at the bottoms of the encoder and decoder stacks. The positional encodings have the same dimension
+# dmodel as the embeddings, so that the two can be summed.
+class PositionalEncoding(nn.Module):
+    "Implement the PE function."
+    def __init__(self, d_model, dropout, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        # Compute the positional encodings once in log space.
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) *
+                             -(math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + Variable(self.pe[:, :x.size(1)],
+                         requires_grad=False)
+        return self.dropout(x)
