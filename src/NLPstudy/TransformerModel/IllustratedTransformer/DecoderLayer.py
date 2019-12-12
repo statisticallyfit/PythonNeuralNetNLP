45f266603bad93cca8dc031bb65b0c36a50364f1
BabitzdaBrave <amg.vintila@gmail.com>
2019-12-12 07:13:02 -0500

    did decoder layer now doing decoder

--- src/NLPstudy/TransformerModel/IllustratedTransformer/DecoderLayer.py
+++ src/NLPstudy/TransformerModel/IllustratedTransformer/DecoderLayer.py
@@ -28,7 +28,7 @@ class DecoderLayer(nn.Module):
     This layer pays attention to Encoder input WHILE DECODING AN OUTPUT.
 
     Query vector is from Decoder self attention layer, BUT KEY AND VALUE vectors are from
-    Encoder output.
+    Encoder output. (See Component 2 calc in forward() )
 
     Args:
         hiddenDim = hidden dimension of model
@@ -36,6 +36,8 @@ class DecoderLayer(nn.Module):
         pffHiddenDim = hidden dimension of positionwise feedforward layer
         attnLayer = SelfAttentionLayer object
         pffLayer = PositionwiseFeedForwardLayer object
+        dropout = amoutn of dropout
+        device = CPU or GPU
 
     '''
 
@@ -52,6 +54,13 @@ class DecoderLayer(nn.Module):
                                                                 numHeads = numHeads,
                                                                 dropout = dropout,
                                                                 device = device)
+
+        # TODO this seems to be the third attention layer
+        self.encoderAttentionLayer: SelfAttentionLayer = attnLayer(hiddenDim = hiddenDim,
+                                                                   numHeads = numHeads,
+                                                                   dropout = dropout,
+                                                                   device = device)
+
         # Creating the poswise feedforward object
         self.poswiseFeedForwardLayer: PositionwiseFeedforwardLayer = \
             pffLayer(hiddenDim = hiddenDim,
@@ -66,40 +75,63 @@ class DecoderLayer(nn.Module):
 
 
 
-    def forward(self, src: Tensor, srcMask: Tensor) -> Tensor:
+    def forward(self, trg: Tensor, src: Tensor, trgMask: Tensor, srcMask: Tensor) -> Tensor:
         '''Get the results of the two components in the encoder layer, the multihead attention
         and the position wise layer
 
         Args:
+            trg =
+                trg shape => (batchSize, trgSentenceLen, hiddenDim)
+            trgMask =
+                trgMask shape => (batchSize, 1, trgSentenceLen, trgSentenceLen)
+
             src:
-                src shape => (batchSize, sentenceLen, hiddenDim)
+                src shape => (batchSize, srcSentenceLen, hiddenDim)
             srcMask:
-                srcMask shape => (batchSize, sentenceLen)
+                srcMask shape => (batchSize, 1, 1, srcSentenceLen)
         '''
 
-        # Component 1: Apply the self attention layer for the src then add the
-        # src(residual) then apply layer normalization since that goes between
+
+
+        # Component 1: Apply the self attention layer for the target then add the
+        # trg(residual) then apply layer normalization since that goes between
         # Component 1 and Component 2
+
         attentionComponent: Tensor = self.layerNormalization(
-            src + self.dropout(
+
+            trg + self.dropout(
                 # Doing forward pass of multihead attention:
-                self.selfAttentionLayer(query = src, key = src, value = src,
-                                        mask = srcMask)
-                # TODO: here is encoder layer yet we are giving mask? Thought only needed in decoder layer?
+                self.selfAttentionLayer(query = trg, key = trg, value = trg,
+                                        mask = trgMask)
             )
         )
 
-        # Component 2: position wise feed forward layer
+        # Component 2: encoder attention is calculated with source as key, values and only
+        # trg as query, since K and V are from Encoder while Q is from Decoder output
+
+        encDecAttentionComponent: Tensor = self.layerNormalization(
+
+            attentionComponent + self.dropout(
+                # Doing forward pass of encoder attention
+                self.encoderAttentionLayer(query = attentionComponent, key = src, value = src,
+                                           mask = srcMask)
+            )
+        )
+
+        # Component 3: position wise feed forward layer of the decoder
         # Apply the positionwise layer for the result of above component 1 step,
         # then add the src(residual), and then
         # apply layer normalization once again, since that goes between components.
+
         poswiseFeedfwdComponent: Tensor = self.layerNormalization(
-            attentionComponent + self.dropout(
+
+            encDecAttentionComponent + self.dropout(
                 # Doing forward pass of position wise feed forward layer:
                 self.poswiseFeedForwardLayer(
-                    X = attentionComponent
+                    X = encDecAttentionComponent
                 )
             )
         )
 
+        # poswise component shape => (batchSize, trgLen, batchSize)
         return poswiseFeedfwdComponent # the entire result
