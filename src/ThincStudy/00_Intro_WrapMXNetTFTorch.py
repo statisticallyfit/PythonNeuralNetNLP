a52f41f85fa314655c0f1f8c372b4ad8951037f4
BabitzdaBrave <amg.vintila@gmail.com>
2020-02-20 08:52:49 -0500

    created pybase env but it is not working via overlap with pythincenv, so created a pythinc env - this time the full thing worked! Completed tutorial 00 for Thinc, starting 01

--- src/ThincStudy/00_Intro_WrapMXNetTFTorch.py
+++ src/ThincStudy/00_Intro_WrapMXNetTFTorch.py
@@ -6,8 +6,8 @@
 # ## Defining Model in Thinc
 
 # %% codecell
-# from thinc.api import prefer_gpu
-# prefer_gpu()  # returns boolean indicating if GPU was activated
+from thinc.api import prefer_gpu
+prefer_gpu()  # returns boolean indicating if GPU was activated
 
 # %% markdown
 # Declaring data below for the whole file:
@@ -15,6 +15,7 @@
 # %% codecell
 import ml_datasets
 
+# note: these are numpy arrays
 (trainX, trainY), (devX, devY) = ml_datasets.mnist()
 print(f"Training size={len(trainX)}, dev size={len(devX)}")
 
@@ -26,13 +27,13 @@ print(f"Training size={len(trainX)}, dev size={len(devX)}")
 # The `chain` combinator: acts like `Sequential` in PyTorch or Keras since it combines a list of layers together with a feed-forward relationship.
 
 # %% codecell
-from thinc.api import chain, Relu, Softmax
+from thinc.api import chain, Relu, Softmax, Model
 
-n_hidden = 32
+numHidden = 32
 dropout = 0.2
 
-model = chain(Relu(nO=n_hidden, dropout=dropout),
-              Relu(nO=n_hidden, dropout=dropout), Softmax())
+model: Model = chain(Relu(nO=numHidden, dropout=dropout),
+              Relu(nO=numHidden, dropout=dropout), Softmax())
 
 # %% codecell
 model
@@ -43,17 +44,22 @@ model
 # Call `Model.initialize` after creating the model and pass in a small batch of input data X and small batch of output data Y. Lets Thinc *infer the missing dimensions* (when we defined the model we didn't tell it the input size `nI` or the output size `nO`)
 #
 # When passing in the data, call `model.ops.asarray` to make sure the data is on the right device (transforms the arrays to `cupy` when running on GPU)
+
 # %% codecell
+from thinc.backends.ops import  ArrayXd
+
+
 # Making sure the data is on the right device
-trainX = model.ops.asarray(trainX)
-trainY = model.ops.asarray(trainY)
-devX = model.ops.asarray(devX)
-devY = model.ops.asarray(devY)
+trainX: ArrayXd = model.ops.asarray(trainX)
+trainY: ArrayXd = model.ops.asarray(trainY)
+devX: ArrayXd = model.ops.asarray(devX)
+devY: ArrayXd = model.ops.asarray(devY)
 
 # Initializing model
 model.initialize(X=trainX[:5], Y=trainY[:5])
-nI = model.get_dim("nI")
-nO = model.get_dim("nO")
+
+nI: int = model.get_dim("nI")
+nO: int = model.get_dim("nO")
 
 print(
     f"Initialized model with input dimension nI = {nI} and output dimension nO = {nO}"
@@ -71,14 +77,16 @@ from tqdm.notebook import tqdm
 def trainModel(data, model, optimizer, numIter: int, batchSize: int):
     (trainX, trainY), (devX, devY) = data
     # todo why need indices?
-    indices = model.ops.xp.arange(trainX.shape[0], dtype="i")
+    # indices = model.ops.xp.arange(trainX.shape[0], dtype="i")
 
     for i in range(numIter):
         # multibatch(): minimatch one or more sequences of data and yield lists with one batch per sequence.
         batches = model.ops.multibatch(batchSize, trainX, trainY, shuffle=True)
 
         for X, Y in tqdm(batches, leave=False):
-            # begin_update(): run the model over a batch of data, returning the output and a callback to complete the backward pass.
+            # begin_update(self, X: InT) -> Tuple[OutT, Callable[[InT], OutT]]:
+            # Purpose: run the model over a batch of data, returning the output and a callback to complete the backward
+            # pass.
             # Returned: tuple (Y, finishedUpdated), where Y = batch of output data, and finishedUpdate = callback that takes the gradient with respect to the output and an optimizer function, and returns the gradient with respect to the input.
             Yh, backprop = model.begin_update(X=X)
 
@@ -89,16 +97,16 @@ def trainModel(data, model, optimizer, numIter: int, batchSize: int):
 
         # Evaluate and print progress
         numCorrect: int = 0
-        total: int = 0
+        totalCount: int = 0
 
         for X, Y in model.ops.multibatch(batchSize, devX, devY):
             # predict(X: InT) -> OutT: calls the model's forward function with is_train=False, and returns only the output, instead of the (output, callback) tuple
             Yh = model.predict(X=X)
             numCorrect += (Yh.argmax(axis=1) == Y.argmax(axis=1)).sum()
             # todo?
-            total += Yh.shape[0]
+            totalCount += Yh.shape[0]
 
-        score = numCorrect / total
+        score = numCorrect / totalCount
 
         print(f" {i}: {float(score):.3f}")
 
@@ -132,7 +140,7 @@ numHidden: int = 32
 dropout: float = 0.2
 
 with Model.define_operators({">>": chain}):
-    model = Relu(nO=numHidden, dropout=dropout) >> Relu(
+    modelByMyOp = Relu(nO=numHidden, dropout=dropout) >> Relu(
         nO=numHidden, dropout=dropout) >> Softmax()
 
 # %% markdown
@@ -157,7 +165,7 @@ nO: int = 10
 nI: int = 784
 dropout: float = 0.2
 
-tfModel = Sequential()
+tfModel: Sequential = Sequential()
 tfModel.add(Dense(width, activation="relu", input_shape=(nI, )))
 tfModel.add(Dropout(dropout))
 tfModel.add(Dense(width, activation="relu", input_shape=(nI, )))
@@ -167,7 +175,7 @@ tfModel
 # %% markdown
 # The wrapped tensorflow model:
 # %% codecell
-wrappedTFModel = TensorFlowWrapper(tensorflow_model=tfModel)
+wrappedTFModel: Model = TensorFlowWrapper(tensorflow_model=tfModel)
 wrappedTFModel
 
 # %% markdown
@@ -176,7 +184,9 @@ wrappedTFModel
 data = ml_datasets.mnist()
 #data
 # %% codecell
-adamOptimizer = Adam(learn_rate=0.001)
+from thinc.optimizers import Optimizer
+
+adamOptimizer: Optimizer = Adam(learn_rate=0.001)
 adamOptimizer
 # %% codecell
 # Providing batch of input data and batch of output data to do shape inference.
@@ -189,6 +199,143 @@ BATCH_SIZE = 128
 
 trainModel(data=data,
            model=wrappedTFModel,
+           optimizer=adamOptimizer,
+           numIter=NUM_ITERATIONS,
+           batchSize=BATCH_SIZE)
+
+# %% markdown
+# ### 2. Wrapping PyTorch Models
+# Thinc's `PyTorchWrapper` wraps the model and turns it into a regular Thinc `Model`.
+
+# %% codecell
+import torch
+import torch.nn
+
+# Renaming imports for reading clarity:
+#import torch.nn.modules.dropout.Dropout2d as Dropout2d
+#import torch.nn.Linear as Linear
+import torch.tensor as Tensor
+import torch.nn.functional as F
+
+# Thinc imports
+from thinc.api import PyTorchWrapper, Adam
+
+width: int = 32
+nO: int = 10
+nI: int = 784
+dropout: float = 0.2
+
+class PyTorchModel(torch.nn.Module):
+    def __init__(self, width: int, nO: int, nI: int, dropout: float):
+        super(PyTorchModel, self).__init__()
+
+        self.firstDropout: torch.nn.Dropout2d = torch.nn.Dropout2d(dropout)
+        self.secondDropout: torch.nn.Dropout2d = torch.nn.Dropout2d(dropout)
+
+        self.firstLinearLayer: torch.nn.Linear = torch.nn.Linear(in_features=nI,
+                                               out_features=width)
+
+        self.secondLinearLayer: torch.nn.Linear = torch.nn.Linear(in_features=width,
+                                                out_features=nO)
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        x: Tensor = F.relu(x)
+        x: Tensor = self.firstDropout(x)
+        x: Tensor = self.firstLinearLayer(x)
+        x: Tensor = F.relu(x)
+        x: Tensor = self.secondDropout(x)
+        x: Tensor = self.secondLinearLayer(x)
+
+        output: Tensor = F.log_softmax(input = x, dim = 1)
+
+        return output
+
+
+wrappedPyTorchModel: Model = PyTorchWrapper(pytorch_model=
+                                            PyTorchModel(width = width,
+                                                         nO = nO,
+                                                         nI = nI,
+                                                         dropout=dropout))
+
+wrappedPyTorchModel
+
+# %% markdown
+# Training the wrapped pytorch model:
+# %% codecell
+
+data = ml_datasets.mnist()
+adamOptimizer: Optimizer = Adam(learn_rate = 0.001)
+
+wrappedPyTorchModel.initialize(X = trainX[:5], Y = trainY[:5])
+wrappedPyTorchModel
+
+# %% codecell
+NUM_ITERATIONS = 10
+BATCH_SIZE = 128
+
+trainModel(data=data,
+           model=wrappedPyTorchModel,
+           optimizer=adamOptimizer,
+           numIter=NUM_ITERATIONS,
+           batchSize=BATCH_SIZE)
+
+
+
+#
+
+
+# %% markdown
+# ### 3. Wrapping MXNet Models
+# Thinc's `MXNetWrapper` wraps the model and turns it into a regular Thinc `Model`.
+#
+# MXNet uses a `.softmax()` method instead of a `Softmax` layer  so to integrate it with the rest of the components, we must combine it with a `Softmax()` Thinc layer using the `chain` combinator.
+# * NOTE: make sure to `initialize()` the MXNet model and the Thinc model (both).
+
+# %% codecell
+from mxnet.gluon.nn import Dense, Sequential, Dropout
+from thinc.api import MXNetWrapper, chain, Softmax
+
+width: int = 32
+nO: int = 10
+nI: int = 784
+dropout: float = 0.2
+
+mxnetModel = Sequential()
+mxnetModel.add(Dense(units = width, activation = "relu"))
+mxnetModel.add(Dropout(rate = dropout))
+mxnetModel.add(Dense(units = width, activation = "relu"))
+mxnetModel.add(Dropout(rate = dropout))
+mxnetModel.add(Dense(units = nO))
+
+mxnetModel
+
+# %% codecell
+mxnetModel.initialize()
+mxnetModel
+
+# %% codecell
+wrappedMxnetModel: Model = chain(layer1 = MXNetWrapper(mxnet_model = mxnetModel),
+                                 layer2 = Softmax())
+wrappedMxnetModel
+
+
+# %% markdown
+# Training the wrapped mxnet model
+# %% codecell
+
+data = ml_datasets.mnist()
+adamOptimizer: Optimizer = Adam(learn_rate = 0.001)
+
+wrappedMxnetModel.initialize(X = trainX[:5], Y = trainY[:5])
+wrappedMxnetModel
+
+# %% codecell
+NUM_ITERATIONS = 10
+BATCH_SIZE = 128
+
+trainModel(data=data,
+           model=wrappedMxnetModel,
            optimizer=adamOptimizer,
            numIter=NUM_ITERATIONS,
            batchSize=BATCH_SIZE)
