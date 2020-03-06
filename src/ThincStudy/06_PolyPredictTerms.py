b805c589928956b78acb986cda5beebef82a76d5
BabitzdaBrave <amg.vintila@gmail.com>
2020-03-06 13:16:59 -0500

    halfway through tut6 thinc

--- src/ThincStudy/06_PolyPredictTerms.py
+++ src/ThincStudy/06_PolyPredictTerms.py
@@ -113,4 +113,266 @@ generatePolyProblems(numLikeTerms = 10)
 
 # %% markdown
 # ## 4. Count Like Terms
-# 
+# Need a function that can count like terms in each problem and return the value for later use as a label.
+#
+# Going to use a few helpers from mathy to enumerate the terms and compare them to see if they are like.
+# %% codecell
+from typing import Optional, List, Dict
+from mathy import MathExpression, ExpressionParser, get_terms, get_term_ex, TermEx
+from mathy.problems import mathy_term_string
+
+
+parser: ExpressionParser = ExpressionParser()
+
+
+def countLikeTerms(inputProblem: str) -> int:
+
+    expression: MathExpression = parser.parse(input_text = inputProblem)
+    termNodes: List[MathExpression] = get_terms(expression = expression)
+    nodeGroups: Dict[str, List[MathExpression]] = {}
+
+    for termNode in termNodes:
+        ex: Optional[TermEx] = get_term_ex(node = termNode)
+
+        # Testing that the unknown variable is not None
+        assert ex is not None, f"invalid expression {termNode}"
+
+        key: str = mathy_term_string(variable = ex.variable,
+                                exponent = ex.exponent)
+
+        # Adding the term node to thenode groups at  key value.
+        if key == "":
+            key = "const"
+        if key not in nodeGroups:
+            # Adding term node list of math exprs for the first time in the dict of node groups at the value key.
+            nodeGroups[key] = [termNode]
+        else:
+            # If the key may be in node groups dict, then append the current term node at that key value.
+            nodeGroups[key].append(termNode)
+
+        # Initializing number of like terms
+    numLikeTerms: int = 0
+
+    for key, termNode in nodeGroups.items():
+        if len(termNode) <= 1:
+            continue
+
+        # Value of nodeGroups is list of math expressions, so now that must contain the number of like terms.
+        numLikeTerms += len(termNode)
+
+    return numLikeTerms
+
+# %% codecell
+# Seeing the expression parser:
+parser
+
+# %% markdown
+# **Try It:**
+# %% codecell
+assert countLikeTerms("4x - 2y + q") == 0
+# %% codecell
+assert countLikeTerms("x + x + z") == 2
+# %% codecell
+assert countLikeTerms("4x + 2x - x + 7") == 3
+
+# %% markdown
+# Counts TOTAL number of terms that are LIKE (so since there are 3 terms with exponent two, 2 terms with exponent one, 2 terms with exponent 3, and only 1 term with exponent six, that results in $3 + 2 + 2 \rightarrow 7$ like terms)
+# %% codecell
+assert countLikeTerms("7x^3 + 8x^6 + 2x + 3x^2 + x^2 + 9x + 9x^3 + 7x^2") == 7
+
+# %% markdown
+# ## 5. Generate Problem / Answer Pairs
+# Now that we can gernate problems, count number of like terms in them, and encode their text to integers, we have the pieces required to generate random problems and answers that we can train a neural network with.
+#
+# Let's write a function that returns a tuple of: the problem text, its encoded example form, and the output label.
+# %% codecell
+from typing import Tuple
+from thinc.api import Ops, get_current_ops
+
+def toExample(inputProblem: str) -> Tuple[str, ModelX, ModelY]:
+    ops: Ops = get_current_ops()
+    encodedInput: ModelX = encodeInput(text = inputProblem)
+    numLikeTerms: int = countLikeTerms(inputProblem = inputProblem)
+
+    # NOTE: the Y vector of type ModelY is just made by vectorizing the number (int) of like terms in the given input problem.
+    return inputProblem, encodedInput, ops.asarray1f([numLikeTerms])
+
+
+# %% markdown
+# **Try It:**
+# %% codecell
+inputProblem, X, Y = toExample(inputProblem = "x + 2x")
+# %% codecell
+inputProblem
+# %% codecell
+X
+# %% codecell
+Y
+
+# %% codecell
+assert X[0] == 46
+assert X[0] == vocab.index("x")
+assert vocab.find('x') == 46
+
+# %% codecell
+assert X[1] == 0
+assert vocab.find(' ') == 0
+assert X[1] == vocab.index(' ')
+
+# %% codecell
+assert X[2] == 2
+assert vocab.find('+') == 2
+assert vocab.index('+') == X[2]
+
+# %% codecell
+assert X[4] == 14
+assert vocab.find('2') == 14
+assert vocab.index('2') == X[4]
+
+# %% markdown
+# ## 6. Build a Model
+# Now that we can generate X, Y values, must define the model and verify it can process a single input / output (using Thinc and the `define_operators` context manager to connect pieces using overloaded operators for `chain` and `clone` operations)
+# %% codecell
+from typing import List
+from thinc.model import Model, OutT
+from thinc.api import concatenate, chain, clone, list2ragged
+from thinc.api import reduce_sum, Mish, with_array, Embed, residual
+
+
+def buildModel(numHiddenLayers: int, dropout: float = 0.1) -> ModelT:
+
+    with Model.define_operators({">>": chain, "|": concatenate, "**": clone}):
+
+        model: Model = (
+            # Iterate over each element in the batch
+            with_array(
+                # Embed the vocab indices
+                layer = Embed(nO = numHiddenLayers, nV = len(vocab), column = 0)
+                # Activate each batch of embedding sequences separately first
+                # Mish = dense layer with Mish activation
+                >> Mish(nO = numHiddenLayers, dropout = dropout)
+            )
+
+            # Convert to ragged so we can use the reduction layers
+            >> list2ragged()
+
+            # Sum the features for each batch input
+            >> reduce_sum()
+
+            # Process with a small resnet
+            >> residual(layer = Mish(nO = numHiddenLayers, normalize = True)) ** 4
+
+            # Convert (batchSize, numHiddenLayers) ==> (batchSize, 1)
+            >> Mish(nO = 1) # todo: so Mish nO acts only on the second dimension? never on the first dimension (batchSize)?
+        )
+
+        return model
+
+
+# %% markdown
+# **Try It:** passing an example through the model to make sure the sizes are being built correctly.
+# %% codecell
+inputProblem, X, Y = toExample(inputProblem = "14x + 2y - 3x + 7x")
+# %% codecell
+inputProblem
+# %% codecell
+X
+assert X.shape == (18, 1)
+assert X.ndim == 2
+# %% codecell
+Y
+# %% codecell
+polyModel: Model = buildModel(numHiddenLayers = 12)
+polyModel
+# %% codecell
+polyModel.initialize(X = [X], Y = polyModel.ops.asarray(Y, dtype="f"))
+# %% codecell
+from thinc.model import OutT
+
+
+yPred: OutT = polyModel.predict(X = [X]) # calls model's forward function
+yPred
+# %% codecell
+assert yPred.shape == (1,1)
+assert yPred.ndim == 2
+
+
+# %% markdown
+# 7. Generate Training Datasets
+# Now that we can generate examples and have model to process them it is time to generate random unique training and evaluation datasets.
+#
+# For this, need to write another helper function to generate $n$ training examples and respects an exclude list to avoid overlaping examples from the training and test sets.
+# %% codecell
+from typing import Tuple, Optional, Set, List
+
+# Assing a type alias
+DatasetTuple = Tuple[List[str], List[ModelX], List[ModelY]]
+
+def generateDataset(size: int, exclude: Optional[Set[str]] = None) -> DatasetTuple:
+
+    ops: Ops = get_current_ops()
+    inputProblemList: List[str] = generatePolyProblems(numLikeTerms = size, exclude = exclude)
+    encodedExamples: List[ModelX] = []
+    outputLabels: List[ModelY] = []
+
+    for i, inputProb in enumerate(inputProblemList):
+        inputProb, x, y = toExample(inputProblem = inputProb)
+        encodedExamples.append(x)
+        outputLabels.append(y)
+
+    return inputProb, encodedExamples, outputLabels
+
+
+# %% markdown
+# **Try It:** Generate a small dataset
+# %% codecell
+inputProblems, x, y = generateDataset(size = 10)
+assert len(inputProblems) == 10
+assert len(x) == 10
+assert len(y) == 10
+# %% codecell
+x
+# %% codecell
+y
+# %% codecell
+inputProblems
+
+
+# %% markdown
+# ## 8. Evaluate Model Performance
+# Need to create a function to check a trained model against a given dataset and return a $0-1$ score of how accurate it was.
+#
+# Use this function to print score as training progresses and print final test prediction at the end of training.
+# %% codecell
+from typing import List
+from wasabi import msg
+
+
+def evaluateModel(model: ModelT, *, isPrintProblems: bool = False, inputProblemList: List[str],
+                  X: List[ModelX], Y: List[ModelY]):
+
+    yEval: ModelY = model.predict(X = X)
+    numCorrect: int = 0
+    numPrint: int = 12
+
+    if isPrintProblems:
+        msg.divider(f"eval samples max({numPrint}")
+
+
+    for inputProb, yAnswer, yGuess in zip(inputProblemList, Y, yEval):
+        # Polishing the guess for Y
+        yGuess = round(float(yGuess))
+        # Record if the guess was correct
+        isCorrect: bool = yGuess == int(yAnswer)
+        printFn = msg.fail
+
+        if isCorrect:
+            numCorrect += 1
+            printFn = msg.good
+        # If not correct ...
+        if isPrintProblems and numPrint > 0:
+            numPrint -= 1
+            printFn(f"Answer[{int(yAnswer[0])}] Guess[{yGuess}] Text: {inputProb}")
+
+    if isPrintProblems:
+        print(f"Model predicted {numCorrect} out of {len(X)} correctly.")
