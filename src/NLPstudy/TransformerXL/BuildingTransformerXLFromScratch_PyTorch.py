9077feb2ee53e23e9c8ff6c1564f726942174b92
BabitzdaBrave <amg.vintila@gmail.com>
2020-03-13 09:08:56 -0400

    working on transformer XL from keitakurita - updated concepts to wiki and some images

--- src/NLPstudy/TransformerXL/BuildingTransformerXLFromScratch_PyTorch.py
+++ src/NLPstudy/TransformerXL/BuildingTransformerXLFromScratch_PyTorch.py
@@ -10,15 +10,32 @@ import torch
 import torch.nn as nn
 import torch.tensor as Tensor
 import matplotlib.pyplot as plt
+# %% codecell
+import sys
+import os
+from IPython.display import Image
+
+# Making files in utils folder visible here:
+sys.path.append(os.getcwd() + "/src/utils/")
+
+# import ImageResizer
+
+# Building pathname for images
+pth = os.getcwd()
+pth
+pth += "/src/NLPstudy/images/"
+pth
 # %% markdown
 # # Building the [Transformer XL](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1513586716) from Sratch
 #
-# ## A Single Attention Head
+# ## A Single [Attention Head](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism)
 # Let us start by implementing a [single attention head](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) in a [`MultiHeadAttention` layer](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1446445463/multi-head+attention+mechanism).
 # ### Assumptions:
 # * Considering the **first** layer only now
 # * Receive an input of [word embeddings](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/87666969) of shape `(seq = 7, batchSize = 3, embeddingDim = 32)`
 # NOTE: the [Transformer XL](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1513586716) does not add [positional embeddings](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1470169419) to the input.
+# %% codecell
+Image(filename = pth + "transformerXL_extendedContext.gif")
 # %% codecell
 from torch.nn import Linear
 
@@ -33,11 +50,11 @@ wordEmbeddings[0,:,:]
 wordEmbeddings[0,:,:].ndim
 wordEmbeddings[0,:,:].shape
 # %% markdown
-# Notes about the transformer XL
-# * [Segment-level recurrence mechanism](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1527480493): we feed the cached outputs of the model for the previous sequence (here this means we are feeding the word embeddings from the previous sequence as an additional input to our model)
-# * Transformer XL does not add the positional embeddings to the input, only in the attention module.
+# #### Notes about the [Transformer XL](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1513586716/transformer-XL+model+ml)
+# * [Segment-level recurrence mechanism](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1527480493): we feed the cached outputs of the model for the previous sequence (here this means we are feeding the [word embeddings](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/87666969/word+embedding+ml) from the previous sequence as an additional input to our model)
+# * [Transformer XL](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1513586716/transformer-XL+model+ml) does not add the [positional embeddings](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1470169419) to the input, only in the [multi-head attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1446445463/multi-head+attention+mechanism) module.
 #
-# As an example, let the previous sequence have length $6$, and let `memory` denote the hidden states (`Tensor`) from the previous sequence.
+# As an example, let the previous sequence have length $6$, and let [`memory`](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1527480493) denote the hidden states (`Tensor`) from the previous sequence.
 # %% codecell
 prevSeqSize: int = 6
 # These are the hidden states from the previous sequence, as an example
@@ -46,12 +63,12 @@ memory
 # %% codecell
 memory.shape
 # %% markdown
-# Each attention head takes keys, queries, and values as input. The procedure in the single attention head is as follows:
+# Each [self-attention head](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) takes keys, queries, and values as input. The procedure in the single [attention head](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) is as follows:
 #
 # 1. Apply a separate linear transformation to each of the keys, queries, and values.
-# 2. Compute the attention scores for each of the values.
-# 3. For each query, compute an attention-weighted sum of the values.
-# 4. Apply a residual connection and layer normalization.
+# 2. Compute the [attention scores](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) for each of the values.
+# 3. For each query, compute an [attention-weighted](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) sum of the values.
+# 4. Apply a [residual connection](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1511358877/residual+connection+layer+ml) and [layer normalization](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1450213381/layer+normalization).
 #
 # ### Step 1: Linear Transformation over $Q, K, V$ matrices
 # %% codecell
@@ -121,7 +138,7 @@ v_tfmd = linearV(wordEmbsWordMemory)
 q_tfmd = linearQ(wordEmbeddings)
 
 # %% markdown
-# Peeking at the dimensions to see how the multiplication resulted in a new tensor shape for the K:
+# Peeking at the dimensions to see how multiplication resulted in a new tensor shape for the matrix $K$:
 # %% codecell
 memory.shape
 # %% codecell
@@ -129,7 +146,7 @@ wordEmbeddings.shape
 # %% codecell
 wordEmbsWordMemory.shape
 # %% markdown
-# For matrix K, first dimension is $13$, as a result of the multiplication of the `linearK` layer with the `wordEmbsWordMemory` whose first dimension is $13$
+# For matrix $K$, first dimension is $13$, as a result of the multiplication of the `linearK` layer with the `wordEmbsWordMemory` whose first dimension is $13$
 # %% codecell
 paramNameAndSizesK
 # %% codecell
@@ -141,12 +158,23 @@ paramNameAndSizesV
 # %% codecell
 v_tfmd.shape
 # %% markdown
-# But first dimension of matrix $Q$ is $7$, as a result of the multiplication of the `linearQ` layer with the `wordEmbeddings` tensor whose first dimension is $7$ not $13$, like that of `wordEmbsWordMemory`
+# But first dimension of matrix $Q$ is $7$, as a result of the multiplication of the `linearQ` layer with the [`wordEmbeddings`](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/87666969/word+embedding+ml) tensor whose first dimension is $7$ not $13$, like that of `wordEmbsWordMemory`
 # %% codecell
 paramNameAndSizesQ
 # %% codecell
 q_tfmd.shape
 
 # %% markdown
-# ### Step 2: Compute Attention Scores for each of the Values
-# Now we compute [scaled dot product attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1671856135/scaled+dot+product+attention) as per the usual [Transformer](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1370095641/transformer+model+ml) model.
+# ### Step 2: Compute [Attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) Scores for each of the Values
+# Now we compute [scaled dot product attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1671856135/scaled+dot+product+attention) as per the usual [Transformer](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1370095641/transformer+model+ml) model. [Scaled dot product attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1671856135/scaled+dot+product+attention) computes the [attention](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1447035008/self+attention+mechanism) score as the dot product between the query and key vectors. (To prevent values from exploding as the dimensionality of the vectors increases, we divide the raw attention score by the sqrt of the [embedding](https://synergo.atlassian.net/wiki/spaces/KnowRes/pages/1474331193/embedding%2Bml) size).
+#
+# $$
+# \text{Attention}(Q, K, V) = \text{softmax}\Bigg( \frac{Q K^T }{\sqrt{d_k}} \Bigg) V
+# $$
+# %% codecell
+Image(filename = pth + "ModalNet-19.png")
+
+# %% markdown
+# TODO: https://stackoverflow.com/a/52874352
+# TODO: trying to get the python `pth` variable to input its value here so no more need for python code cells to show images, can use just markdown
+# ![Scaled Dot Product Attention](pth + ModalNet-19.png)
