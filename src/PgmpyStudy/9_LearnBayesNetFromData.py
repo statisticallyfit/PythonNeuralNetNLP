6b35614fc890c59d907a79e644f13fe1ea222334
BabitzdaBrave <amg.vintila@gmail.com>
2020-05-12 03:22:28 -0400

    done tutorial 9 pgmpy, creating its notebook now

--- src/PgmpyStudy/9_LearnBayesNetFromData.py
+++ src/PgmpyStudy/9_LearnBayesNetFromData.py
@@ -77,7 +77,7 @@ from src.utils.NetworkUtil import *
 
 import pandas as pd
 from pandas import DataFrame
-# %% markdown
+# %% markdown [markdown]
 # ## Parameter Learning
 # Supposed we have the following data:
 # %% codecell
@@ -96,7 +96,7 @@ fruitModel = BayesianModel([('fruit', 'tasty'), ('size', 'tasty')])
 pgmpyToGraph(fruitModel)
 
 
-# %% markdown
+# %% markdown [markdown]
 # ### 1/ State Counts
 # To make sense of the given data we can count how often each state of the variable occurs. If the variable is dependent on parents, the counts are done conditionally on the parents' states, so separately for each parent configuration.
 # %% codecell
@@ -107,10 +107,10 @@ pe: ParameterEstimator = ParameterEstimator(model = fruitModel, data = fruitData
 print(pe.state_counts(variable = 'fruit')) # example of unconditional state counts
 print('\n', pe.state_counts('tasty')) # example of conditional count of fruit and size
 
-# %% markdown
+# %% markdown [markdown]
 # Can see that as many apples as bananas were observed and that $5$ large bananas were tasty while the only small one was not.
 
-# %% markdown
+# %% markdown [markdown]
 # ### 2/ Maximum Likelihood Estimation
 # A natural estimate for the CPDs is to use the *relative frequencies* (probabilities version of the state count table above). For instance we observed $7$ apples among a total of $14$ fruits, so we might guess that about half the fruits are apples.
 #
@@ -132,7 +132,7 @@ print(estCPD_size)
 estCPD_tasty: TabularCPD = mle.estimate_cpd('tasty') # conditional
 print(estCPD_tasty)
 
-# %% markdown
+# %% markdown [markdown]
 # The `mle.get_parameters()` method returns a list of CPDs for all variables of the model.
 #
 # The `fit()` method of `BayesianModel` provides more convenient access to parameter estimators:
@@ -146,7 +146,7 @@ assert (estCPDs[0] == estCPD_fruit and
 for cpd in estCPDs:
     print(cpd)
 
-# %% markdown
+# %% markdown [markdown]
 # #### $\color{red}{\text{Problem with MLE Estimation: }}$
 # The MLE estimator has the problem of *overfitting* to the data. In the above CPD, the probability of a large banance being tasty is estimated at $0.833$ because $5$ out of $6$ observed large bananas were tasty. Ok, but note that the probability of a small banana being tasty is estimated at $0.0$ because we **observed ONLY ONE small banana** and it **happened not to be tasty**.
 #
@@ -182,7 +182,7 @@ bayesEstCPD_tasty: TabularCPD = est.estimate_cpd(node = 'tasty',
 print("New Bayes CPD: \n", bayesEstCPD_tasty)
 # %% codecell
 print("Old MLE CPD: \n", estCPD_tasty)
-# %% markdown
+# %% markdown [markdown]
 # The estimated CPD values are more conservative. In particular, the estimate for a small banana being not tasty is now around $0.64$ instead of $1.0$. Setting `euiqvalent_sample_size` = $10$ means that for each parent configuration, we add the equivalent of $10$ uniform samples (meaning $+5$ small bananas are seen as tasty and $+5$ are not tasty)
 #
 # #### Fitting Model CPDs with `BayesianEstimator`:
@@ -194,7 +194,7 @@ import numpy as np
 
 assert fruitModel.get_cpds() == [], "Check the cpds are empty beforehand"
 
-fruitModel.fit(data, estimator = BayesianEstimator,
+fruitModel.fit(fruitData, estimator = BayesianEstimator,
                prior_type = 'BDeu',
                equivalent_sample_size= 10) # default equivalent_sample_size = 5
 
@@ -206,11 +206,11 @@ assert (bayesEstCPD_fruit == bayesCPDs[0] and
 for cpd in bayesCPDs:
     print(cpd)
 
-# %% markdown
+# %% markdown [markdown]
 # The `fruitModel` with estimated CPDs using `BayesianEstimator`:
 # %% codecell
 pgmpyToGraphCPD(fruitModel)
-# %% markdown
+# %% markdown [markdown]
 # Another example using `fit()` with `BayesianEstimator`:
 # %% codecell
 # Generate data:
@@ -226,7 +226,7 @@ for cpd in model.get_cpds():
 # %% codecell
 pgmpyToGraphCPD(model)
 
-# %% markdown
+# %% markdown [markdown]
 # ## Structure Learning
 # To learn model structure (a DAG) from a data set, there are two broad techniques:
 # * score-based structure learning
@@ -274,7 +274,7 @@ k2.score(commonEvidenceModel)
 # %% codecell
 bic.score(commonEvidenceModel)
 
-# %% markdown
+# %% markdown [markdown]
 # The `commonEvidenceModel` is the correct model for the data relationship $ Z = X + Y$ and scores higher than the `commonCauseModel`, as expected.
 #
 # $\color{red}{\text{TODO: }}$ why higher BIC scores good here? Thought lower BIC is good, which would indicated `commmonCauseModel` is better...?
@@ -285,7 +285,7 @@ k2.score(commonCauseModel)
 # %% codecell
 bic.score(commonCauseModel)
 
-# %% markdown
+# %% markdown [markdown]
 # * KEY NOTE: these scores *decompose* so they can be computed locally for each of the variables, **given** their **potential parents**, while **independent** of other parts of the network:
 # %% codecell
 bdeu.local_score(variable = 'Z', parents = [])
@@ -293,7 +293,7 @@ bdeu.local_score(variable = 'Z', parents = [])
 bdeu.local_score(variable = 'Z', parents = ['X'])
 # %% codecell
 bdeu.local_score(variable = 'Z', parents = ['X', 'Y'])
-# %% markdown
+# %% markdown [markdown]
 # The local score is highest when both parents are considered, which reflects the data relation $Z = X + Y$
 #
 # **Example 2:** Fruit Data
@@ -332,7 +332,7 @@ print(k2Fruit.local_score(variable = 'tasty', parents = ['size', 'fruit']))
 print(bicFruit.local_score(variable = 'tasty', parents = ['size', 'fruit']))
 
 
-# %% markdown
+# %% markdown [markdown]
 # ### Search Strategies
 # The search space of DAGs is super-exponential in the number of variables and the above scoring functions allow for local maxima. The first property makes exhaustive search intractable for all but very small networks, the second prohibits efficient local optimization algorithms to always find the optimal structure. Thus, identifiying the ideal structure is often not tractable. Despite these bad news, heuristic search strategies often yields good results.
 #
@@ -359,7 +359,7 @@ print("All DAGs sorted by score:\n")
 
 for score, dag in reversed(es.all_scores()):
     print(f"Score = {score},   Edges: {dag.edges()}")
-# %% markdown
+# %% markdown [markdown]
 # Drawing a few of the highest-scoring ones and lowest-scoring ones
 # %% codecell
 from networkx.classes.digraph import DiGraph
@@ -390,7 +390,7 @@ pgmpyToGraph(scoresAndDags[LEN-1][1])
 # Second-worst graph:
 pgmpyToGraph(scoresAndDags[LEN-2][1])
 
-# %% markdown
+# %% markdown [markdown]
 # **Example 2:* Fruit data
 # %% codecell
 es: ExhaustiveSearch = ExhaustiveSearch(data = fruitData, scoring_method = bicFruit)
@@ -399,7 +399,7 @@ bestFruitModel: DAG = es.estimate()
 bestFruitModel.edges()
 # TODO why is this empty?
 
-# %% markdown
+# %% markdown [markdown]
 # #### Heuristic Search
 # Once more ndoes are involved we need to switch to heuristic search. The `HillClimbSearch` implements a greedy local search that starts from the DAG `start` (default disconnected DAG) and proceeds by iteratively performing single-edge manipulations that maximally increase the score. The search terminates once a local maximum is found.
 #
@@ -422,7 +422,7 @@ bestModel.edges()
 # %% codecell
 pgmpyToGraph(bestModel)
 
-# %% markdown
+# %% markdown [markdown]
 # The search correctly identifies that $B$ and $C$ do not influence $H$ directly, only through $A$ and of course that $D$, $E$, $F$ are independent.
 #
 # To enforce a wider exploration of the search space, the search can be enhanced with a tabu list. The list keeps track of the last n modfications; those are then not allowed to be reversed, regardless of the score. Additionally a `white_list` or `black_list` can be supplied to restrict the search to a particular subset or to exclude certain edges. The parameter `max_indegree` allows to restrict the maximum number of parents for each node.
@@ -439,7 +439,7 @@ bestFruitModel.edges()
 
 
 
-# %% markdown
+# %% markdown [markdown]
 # ### Constraint-Based Structure Learning
 # A different but straightforward approach to build a DAG from data is:
 # 1. Identify independencies in the data set using hypothesis tests
@@ -472,7 +472,7 @@ assert est.test_conditional_independence('A', 'G')
 assert not est.test_conditional_independence('A', 'G', Zs = ['H'])
 assert not est.test_conditional_independence('A', 'H', Zs = ['G'])
 
-# %% markdown
+# %% markdown [markdown]
 # `test_conditional_independence()` returns a triple `(chi2, pValue, sufficientData)` consisting of the computed $\chi$-squared test statistic, the `pValue` of the test, and a heuristic flag that indicates if the sample size was sufficient. The `pValue` is the probability of observing the computed $\chi$-squared statistic (or an even higher $\chi$-squared value) given the null hypothesis that $X$ and $Y$ are independent given $Z$s.
 #
 # $\color{red}{\text{TODO: this is not true!}}$
@@ -480,13 +480,13 @@ assert not est.test_conditional_independence('A', 'H', Zs = ['G'])
 # #### DAG (pattern) Construction
 # Can now construct a DAG from the data set in three steps:
 #
-# 1. Construct an undirected skeleton using `estimate_skeleton()`. The job of `estimate_skeleton()` is: to estimate a graph skeleton (UndirectedGraph) for the data set. Uses the `build_skeleton` method (PC algorithm); independencies are determined using a chisquare statistic with the acceptance threshold of `significance_level`. Returns
+# 1. `estimate_skeleton()`: Construct an undirected skeleton using `estimate_skeleton()`. The job of `estimate_skeleton()` is: to estimate a graph skeleton (UndirectedGraph) for the data set. Uses the `build_skeleton` method (PC algorithm); independencies are determined using a chisquare statistic with the acceptance threshold of `significance_level`. Returns
 #    * `skeleton`: `UndirectedGraph` = An estimate for the undirected graph skeleton of the BN underlying the data.
 #    * `separating_sets`: dict = A dict containing for each pair of not directly connected nodes a separating set of variables that makes them conditionally independent. (needed for edge orientation procedures)
-# 2. Orient compelled edges to obtain partially directed acyclic graph (PDAG, I-equivalence class of DAGs) using `skeleton_to_pdag()`, which takes the outputted `skeleton` and `separating_sets` to create a DAG pattern. Returns:
-#       * pdag: DAG = An estimate for the DAG pattern of the BN underlying the data. The graph might contain some nodes with both-way edges (X->Y and Y->X). Any completion by (removing one of the both-way edges for each such pair) results in a I-equivalent Bayesian network DAG.
-# 3. Extend DAG pattern to a DAG by conservatively orienting the remaining edges in some way, using `pdag_to_dag()`.Completes a PDAG to a DAG, without adding v-structures, if such a completion exists. If no faithful extension is possible, some fully oriented DAG that corresponds to the PDAG is returned and a warning is generated. This is a static method. Returns:
-#       * dag: DAG = A faithful orientation of pdag, if one exists. Otherwise any fully orientated DAG/BayesianModel with the structure of pdag.
+# 2. `skeleton_to_pdag()`: Orient compelled edges to obtain partially directed acyclic graph (PDAG, I-equivalence class of DAGs) using `skeleton_to_pdag()`, which takes the outputted `skeleton` and `separating_sets` to create a DAG pattern. Returns:
+#       * pdag: `DAG` = An estimate for the DAG pattern of the BN underlying the data. The graph might contain some nodes with both-way edges (X->Y and Y->X). Any completion by (removing one of the both-way edges for each such pair) results in a I-equivalent Bayesian network DAG.
+# 3. `pdag_to_dag()`: Extend DAG pattern to a DAG by conservatively orienting the remaining edges in some way, using `pdag_to_dag()`.Completes a PDAG to a DAG, without adding v-structures, if such a completion exists. If no faithful extension is possible, some fully oriented DAG that corresponds to the PDAG is returned and a warning is generated. This is a static method. Returns:
+#       * dag: `DAG` = A faithful orientation of pdag, if one exists. Otherwise any fully orientated DAG/BayesianModel with the structure of pdag.
 #
 # PDAGs are `DirectedGraph`s that may contain both-way edges, to indicate that the orientation for the edge is not determined.
 # %% codecell
@@ -519,6 +519,111 @@ print("DAG edges: ", model.edges())
 pgmpyToGraph(model)
 
 
-# %% markdown
+# %% markdown [markdown]
 # The `estimate()` method gives a shorthand for the three steps and directly reutrns a `BayesianModel`:
+#
+# $\color{red}{\text{TODO}}$ it is not actually a `BayesianModel` it is still a `DAG`
 # %% codecell
+estModel: DAG = est.estimate(significance_level = 0.01)
+
+assert estModel.edges() == model.edges(), "Check: both methods of getting the estimated DAG are equivalent"
+
+estModel.edges()
+# %% codecell
+pgmpyToGraph(estModel)
+
+# %% markdown [markdown]
+# The `estimate_from_independencies()` method can be used to create a `BayesianModel` from a provided *set of independencies*.
+#
+# Estimates a DAG from an `Independencies()`-object or a decision function for conditional independencies. This requires that the set of independencies admits a faithful representation (e.g. is a set of d-separation for some BN or is closed under the semi-graphoid axioms).
+#
+# * NOTE: **Meaning of Faithful**: PC PDAG construction is only guaranteed to work under the assumption that the identified set of independencies is **faithful**, i.e. there exists a DAG that exactly corresponds to it. Spurious dependencies in the data set can cause the reported independencies to violate faithfulness. It can happen that the estimated PDAG does not have any faithful completions (i.e. edge orientations that do not introduce new v-structures). In that case a warning is issued.
+# %% codecell
+ind: Independencies = Independencies(['B', 'C'], ['A', ['B', 'C'], 'D'])
+
+indClosure: Independencies = ind.closure()  #required for faithfulness
+
+indModel = ConstraintBasedEstimator.estimate_from_independencies(nodes = "ABCD", independencies = indClosure)
+
+indModel.edges()
+
+# %% codecell
+pgmpyToGraph(indModel)
+
+
+
+
+# %% markdown [markdown]
+# ### Hybrid Structure Learning
+# The MMHC algorithm combines constraint-based and score-based structure learning methods. It has two parts:
+#
+# 1. Learn an undirected graph skeleton using the constraint-based construction procedure MMPC
+# 2. Orient edges using score-based optimization (BDeu score + modified hill-climbing)
+#
+# Can perform these two steps somewhat separately:
+# %% codecell
+from pgmpy.estimators import MmhcEstimator
+from pgmpy.estimators import BDeuScore
+
+data: DataFrame = DataFrame(data = np.random.randint(low = 0, high = 3, size=(2500, 8)),
+                            columns = list("ABCDEFGH"))
+
+data['A'] += data['B'] + data['C']
+data['H'] = data['G'] - data['A']
+data['E'] *= data['F']
+
+
+# %% markdown [markdown]
+# **Part 1:** Learning the Unidirected graph skeleton using `MMPC` algorithm (constraint-based method)
+
+# %% codecell
+mmhc: MmhcEstimator = MmhcEstimator(data = data)
+
+
+### Part 1) Skeleton structure estimation
+# Estimates a graph skeleton (UndirectedGraph) for the data set, using then MMPC (max-min parents-and-children) algorithm.
+skeleton: UndirectedGraph = mmhc.mmpc()
+
+
+print("part 1) skeleton: ", skeleton.edges())
+# Remember this is supposed to be UNIDRECTED graph so ignore the directions
+pgmpyToGraph(skeleton)
+
+# %% markdown [markdown]
+# * **NOTE:** Showing the meaning of `to_directed()` method in the next code (used to create the hybrid model)
+# %% codecell
+# NOTE: the to_directed() method makes a mirror image of the existing edge. So if there is E --> D then the method also adds D --> E.
+print("Skeleton (undirected) edges: ", list(iter(skeleton.edges())) ) #== [('A', 'H'), ('D', 'E'), ('E', 'F'), ('G', 'H')]
+
+print("Skeleton (directed) edges: ", list(iter(skeleton.to_directed().edges())) ) #== [('A', 'H'), ('D', 'E'), ('E', 'D'), ('E', 'F'), ('F', 'E'), ('G', 'H'), ('H', 'A'), ('H', 'G')]
+
+# %% codecell
+pgmpyToGraph(skeleton.to_directed())
+
+# %% markdown [markdown]
+# **Part 2:** Orienting Edges using Score-Based Optimization
+# %% codecell
+# Use hill climb search to orient the edges
+hc: HillClimbSearch = HillClimbSearch(data = data, scoring_method = BDeuScore(data))
+
+### Part 2) Model estimation
+# NOTE: providing the white list argument limits the search to edges only in white list
+modelHybrid: DAG = hc.estimate(tabu_length = 10,
+                               white_list = skeleton.to_directed().edges())
+
+
+
+print("Part 2) Model: ", modelHybrid.edges())
+
+pgmpyToGraph(modelHybrid)
+
+# %% markdown [markdown]
+# `MmhcEstimator.estimate()` combines both these steps and directly estimates a `BayesianModel`.
+#
+# Creating the model the short way:
+# %% codecell
+modelMMHC: DAG = mmhc.estimate(scoring_method = BDeuScore(data), tabu_length = 10)
+
+assert modelMMHC.edges() == modelHybrid.edges()
+
+pgmpyToGraph(modelMMHC)
