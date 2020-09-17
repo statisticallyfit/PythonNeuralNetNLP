356444ccc2d55ad76a9c479eb1d6d9599e629af7
statisticallyfit <statisticallyfit@gmail.com>
2020-09-17 10:04:13 +0300

    trying to make n_ij args shorter with matrix symbol

--- src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/ch1_again.py
+++ src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/ch1_again.py
@@ -1,5 +1,5 @@
 # %% codecell
-from sympy import Matrix, Symbol, derive_by_array, Lambda, Function
+from sympy import Matrix, Symbol, derive_by_array, Lambda, Function, MatrixSymbol
 from sympy.abc import x, i, j, a, b
 
 # %% markdown
@@ -49,13 +49,50 @@ Derivative(h, f(x)).doit()
 
 
 # %% codecell
-n_ij = Function('n_ij')(N[0,0]); n_ij
-n_ij.args[0]
+A = MatrixSymbol('X',3,3); Matrix(A)
+B = MatrixSymbol('W',3,2)
+# %% codecell
+n_ij = Function('n_ij')(A,B); n_ij # (N[0,0]); n_ij
+n_ij.args
+
 N[0,0].args
+# %% codecell
+# sigma(n_ij).diff(n_ij).replace(n_ij, N[0,0]) # ERROR cannot deriv wi.r.t to the expression w11*x11 + ...
+
+# %% codecell
+sigma(n_ij).diff(A)
 # %% codecell
 sigma(n_ij).diff(n_ij)
 # %% codecell
+sigma(n_ij).diff(Matrix(X))
+
+# %% codecell
+
+sigma(n_ij).diff(n_ij)
+# %% codecell
+sigma(n_ij).diff(n_ij).subs({n_ij: N[0,0]})
+
+
+
+
+
+
+# %% codecell
+# ### WAY 2:
+n_ij = Function('n_11')(N[0,0]); n_ij
+
+# %% codecell
+sigma(n_ij)
+# %% codecell
+sigma(n_ij).subs({n_ij : n_ij.args[0]})
+# %% codecell
+sigma(n_ij).diff(n_ij) #.replace(n_ij, n_ij.args[0])
+# %% codecell
+sigma(n_ij).diff(n_ij).subs({n_ij : n_ij.args[0]})
+# %% codecell
 sigma(n_ij).diff(X[0,0])
 # %% codecell
+sigma(n_ij).diff(X[0,0]).subs({n_ij: n_ij.args[0]})
+
 # %% codecell
 # %% codecell
