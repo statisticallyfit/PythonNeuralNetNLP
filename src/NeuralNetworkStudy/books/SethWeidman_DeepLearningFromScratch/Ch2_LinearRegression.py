0c8c0076882e79d6149d137576b43bfaf342a657
statisticallyfit <statisticallyfit@gmail.com>
2020-11-15 00:06:28 +0200

    started chapter 2 in Seth, working on markdown for regression (linear) formulation; yet to do the linear regr code and then will follow the nonlinear reg from neural nets

--- src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/Ch2_LinearRegression.py
+++ src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/Ch2_LinearRegression.py
@@ -72,4 +72,63 @@ from sympy.interactive import printing
 printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLatexPrinter.doprint(e))
 
 
-# %% 
+# %% markdown
+# ## Linear Regression Formulas
+# 
+# ### Data Representation
+# 
+# The data is represented in a matrix $X$ with $n$ rows, each of which represents an observation with $k$ features, all of which are numbers. Each row observation is a vector $\overrightarrow{x}_i = \begin{pmatrix}x_{i1}  &  x_{i2}  &  x_{i3} & ... & x_{ik} \end{pmatrix}$. Each of these row observation vectors will be stacked on top of one another to form a $b \times k$ batch matrix, $X_{\text{batch}}$. 
+# 
+# For instance a batch of size $b$ would look like this: 
+# $$
+# \begin{aligned}
+# X_\text{batch} = \begin{pmatrix}
+#   x_{11} & x_{12} & ... & x_{1k} \\
+#   x_{21} & x_{22} & ... & x_{2k} \\
+#   \vdots & \vdots & ... & \vdots \\
+#   x_{b1} & x_{b2} & ... & x_{bk} 
+# \end{pmatrix}
+# = \begin{pmatrix}
+# \overrightarrow{x}_1 \\ \overrightarrow{x}_2 \\ \vdots \\ \overrightarrow{x}_b
+# \end{pmatrix}
+# \end{aligned}
+# $$
+# where ... 
+# * $k$ = number of data features or predictors
+# * $n$ = number of rows or number of data observations
+# * $b$ = number of batches
+# 
+# 
+# $\color{red}{\text{TODO question: In the entire data matrix } X \text{ there can be many batches } b \text{ fit inside the } n \text{ rows. Does it make sense to think of } X \text{ as at least a 3-dimensional tensor ?? }}$
+# 
+# 
+# ### Target Representation
+# 
+# For each batch of observations $X_{\text{batch}}$ there is a corresponding batch of *targets*, each element of which is the target number for the corresponding observation. For instance $\overrightarrow{y}_i$ is the target number for observation vector $\overrightarrow{x}_i$. 
+# 
+# We can represent that batch of targets for $X_\text{batch}$ in a one-dimensional vector, $b \times 1$ vector: 
+# 
+# $$
+# \overrightarrow{y}_\text{batch} = \begin{pmatrix}
+#   y_1 \\
+#   y_2 \\
+#   \vdots \\
+#   y_b
+# \end{pmatrix}
+# $$
+# where $b$ = batch size. 
+# 
+# 
+# ## Prediction Representation
+# The goal of supervised learning is to build a function that takes input batches of observations with the structure of $X_\text{batch}$ and produce vectors of predictions $\overrightarrow{p}_\text{batch} = \begin{pmatrix} p_1 \\ p_2 \\ \vdots \\ p_b \end{pmatrix}$ with values $p_i$. Each of these prediction numbers $p_i$ should be close to the target values $y_i$. 
+# 
+# 
+# ## Linear Regression Formulation
+# 
+# Linear regression is often shown as: 
+# $$
+# \overrightarrow{y}_i = \beta_0 + \beta_1 \times \overrightarrow{x}_1 + ... + \beta_k \times \overrightarrow{x}_k + \epsilon
+# $$
+# 
+# This representation describes the belief that the numeric value of each target is a linear combination of the $k$ features (predictors) of $X$, where the $\beta_0$ term adjusts the "baseline" value of the prediction (this means it is the prediction made when all other features equal $\overrightarrow{0}$).
+# %% codecell
