47705fff832ab4bb391effa587938cd5c2e03de3
statisticallyfit <statisticallyfit@gmail.com>
2020-11-21 00:15:21 +0200

    did alphaSubs function to put the matrix deriv result right into alpha (will need to do same to lambda), but 2) also trying to result just in X^T from the sigma applyfunc expr

--- src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/Ch2_LinearRegression.py
+++ src/NeuralNetworkStudy/books/SethWeidman_DeepLearningFromScratch/Ch2_LinearRegression.py
@@ -19,7 +19,7 @@ from typing import *
 import itertools
 from functools import reduce
 
-from sympy import Matrix, Symbol, derive_by_array, Lambda, Function, MatrixSymbol, Identity, Derivative, symbols, diff, HadamardProduct
+from sympy import Matrix, Symbol, derive_by_array, Lambda, Function, MatrixSymbol, Identity,  Derivative, symbols, diff, HadamardProduct
 from sympy.abc import x, i, j, a, b
 
 # %%
@@ -229,7 +229,7 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 #
 #
 # ## Training the Model
-# 
+#
 # To compare the predictions to a standard, we need to use a vector of *targets* $\overrightarrow{y}_\text{batch}$ associated with the batch of observations $X_\text{batch}$ fed into the function. Then we compute a single number that is a function of $\overrightarrow{y}_\text{batch}$ and $\overrightarrow{p}_\text{batch}$ that represents the model's penalty for making predictions that it did. We can choose the **mean squared error** which is the average squared value that our model's predictions were off target:
 # $$
 # \begin{aligned}
@@ -247,32 +247,32 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 
 # %% [markdown]
 # ## Linear Regression: The Code
-# 
+#
 # Computing derivatives for nested functions using the chain rule involves two sets of steps: **first** we perform a "forward pass", passing the input successively forward through a series of operations and saving the quantities computed as we go; **second**, we use those quantities to compute the appropriate derivatives during the "backward pass".
 #
-# 
+#
 # ## Forward Pass for Linear Regression
-# 
+#
 # Below in the code, the forward function computes quantities in the forward pass and saves them in a dictionary, which serves the additional purpose of differentiating between forward pass quantities computed her and the parameters themselves.
-# 
-# The equations that are computed in the forward pass are as follows: 
-# 
-# Let: 
+#
+# The equations that are computed in the forward pass are as follows:
+#
+# Let:
 # * $k$ = number of data features or predictors
 # * $n$ = number of rows or number of data observations
 # * $b$ = number of batches
-# 
-# 
+#
+#
 # ### Step 1: Matrix Multiplication: $N = \nu(X, \overrightarrow{w}) = X \times \overrightarrow{w}$
-# 
-# 
+#
+#
 # Assume,
 # * $X_\text{batch} \in \mathbb{R}^{b \times k}$
 # * $\overrightarrow{w} \in \mathbb{R}^{k \times 1}$
 # * $\nu : \mathbb{R}^{(b \times k) \times (k \times 1)} \rightarrow \mathbb{R}^{b \times 1}$
 # * $N = \overrightarrow{N} \in \mathbb{R}^{b \times 1}$
-# 
-# 
+#
+#
 # Multiply the matrix $X_\text{batch}$ and the vector of parameters $\overrightarrow{w}$
 # $$
 # \begin{aligned}
@@ -296,22 +296,22 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 #   x_{21} \cdot w_1  & x_{22} \cdot w_2  & ... & x_{2k} \cdot w_k  \\
 #   \vdots & \vdots & ... & \vdots \\
 #   x_{b1} \cdot w_1  & x_{b2} \cdot w_2  & ... & x_{bk} \cdot w_k
-# \end{pmatrix} 
+# \end{pmatrix}
 # \end{aligned}
 # $$
 
 # %% [markdown]
 # ### Step 2: Predictions: $P = \alpha(N, B) = N + B$
-# 
-# 
-# Assume: 
+#
+#
+# Assume:
 # * $N = \overrightarrow{N} \in \mathbb{R}^{b \times 1}$
 # * $B = \overrightarrow{\beta_0} \in \mathbb{R}$ so this intercept is a constant.
 # $\color{red}{\text{TODO: vector intercept issue to fix}}$
 # * $\alpha: \mathbb{R}^{(b \times 1) \times (1 \times 1)} \rightarrow \mathbb{R}^{b \times 1}$
 # * $\overrightarrow{P} \in \mathbb{R}^{b \times 1}$
-# 
-# 
+#
+#
 # $$
 # \begin{aligned}
 # \overrightarrow{P} &= \alpha(N, B) \\
@@ -347,8 +347,8 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 
 # %% [markdown]
 # ### Step 3: Loss: $L = \Lambda(P, Y) = \text{MSE}(P, Y)$
-# 
-# Assume: 
+#
+# Assume:
 # * $X \in \mathbb{R}^{b \times k}$
 # * $\overrightarrow{w} \in \mathbb{R}^{k \times 1}$
 # * $B = \overrightarrow{\beta_0} \in \mathbb{R}$ so this intercept is a constant.
@@ -359,9 +359,9 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 # * $P = \overrightarrow{P} \in \mathbb{R}^{b \times 1}$
 # * $\Lambda: \mathbb{R}^{(b \times 1) \times (b \times 1)} \rightarrow \mathbb{R}$
 # * $L \in \mathbb{R}$
-# 
-# 
-# 
+#
+#
+#
 # $$
 # \begin{aligned}
 # L &= \Lambda(\alpha(\nu(X, \overrightarrow{w}), B), Y) \\
@@ -371,16 +371,16 @@ printing.init_printing(use_latex='mathjax', latex_printer= lambda e, **kw: myLat
 #   &= \text{MSE} \Bigg( \begin{pmatrix} p_1 \\ p_2 \\ \vdots \\ p_b \end{pmatrix}, \begin{pmatrix} y_1 \\ y_2 \\ \vdots \\ y_b \end{pmatrix} \Bigg) \\
 #   &= \frac{(\overrightarrow{P} - \overrightarrow{Y})^2} {b} \\
 #   &= \frac {\begin{pmatrix}
-# \begin{pmatrix}  y_1 \\ y_2 \\ \vdots \\ y_b \end{pmatrix} 
-# - 
+# \begin{pmatrix}  y_1 \\ y_2 \\ \vdots \\ y_b \end{pmatrix}
+# -
 # \begin{pmatrix}  p_1 \\ p_2 \\ \vdots \\ p_b \end{pmatrix}
 #   \end{pmatrix}^2 } {b} \\
 #   &= \frac {(y_1 - p_1)^2 + (y_2 - p_2)^2 + ... + (y_b - p_b)^2 } {b}
 # \end{aligned}
 # $$
-# 
-# 
-# * **NOTE:** in the above steps, we use $N := \overrightarrow{N}$, $P := \overrightarrow{P} := \overrightarrow{p}_\text{batch_with_bias}$ and $Y := \overrightarrow{Y} := \overrightarrow{y}_\text{batch}$ and $X := X_\text{batch}$. 
+#
+#
+# * **NOTE:** in the above steps, we use $N := \overrightarrow{N}$, $P := \overrightarrow{P} := \overrightarrow{p}_\text{batch_with_bias}$ and $Y := \overrightarrow{Y} := \overrightarrow{y}_\text{batch}$ and $X := X_\text{batch}$.
 #   * $\color{red}{\text{Reason (?) maybe because we want to say that more batches can fit inside each of the symbols X, P, Y ...}}$
 
 # %% codecell
@@ -425,22 +425,22 @@ def forwardLinearRegression(X_batch: Tensor, y_batch: Tensor, weights: Dict[str,
 
     ### Compute the forward pass operations
     N_batch: Tensor = X_batch.matmul(w)
-    # torch.mv() only works if w.shape == (dim1, ) instaed of (dim1, dim2) so we have to use matmul here, even though w is a vector so dim2 == 1. 
+    # torch.mv() only works if w.shape == (dim1, ) instaed of (dim1, dim2) so we have to use matmul here, even though w is a vector so dim2 == 1.
 
-    # TODO: In pytorch it IS possible to add a tensor and scalar together but in a vector space it is NOT possible ... 
-    P_batch_with_bias: Tensor = N_batch + beta_0 
+    # TODO: In pytorch it IS possible to add a tensor and scalar together but in a vector space it is NOT possible ...
+    P_batch_with_bias: Tensor = N_batch + beta_0
 
-    # This is the MSE formula: 
+    # This is the MSE formula:
     loss: Tensor = torch.mean(torch.pow(y_batch - P_batch_with_bias, 2))
 
     # Save the information computed on the forward pass
     forwardInfo: Dict[str, Tensor] = {}
-    forwardInfo['X'] = X_batch 
-    forwardInfo['N'] = N_batch 
-    forwardInfo['P'] = P_batch_with_bias 
-    forwardInfo['y'] = y_batch 
+    forwardInfo['X'] = X_batch
+    forwardInfo['N'] = N_batch
+    forwardInfo['P'] = P_batch_with_bias
+    forwardInfo['y'] = y_batch
 
-    return loss, forwardInfo 
+    return loss, forwardInfo
 
 
 
@@ -449,44 +449,44 @@ def forwardLinearRegression(X_batch: Tensor, y_batch: Tensor, weights: Dict[str,
 
 # %% [markdown]
 # ## Training the Model: The Code
-# 
-# We must compute $\frac{\partial L} {\partial w_i}$ for every parameter $w_i$ in the vector of parameters $\overrightarrow{w}$, and we must additionally compute $\frac{\partial L}{\partial \beta_0}$. 
+#
+# We must compute $\frac{\partial L} {\partial w_i}$ for every parameter $w_i$ in the vector of parameters $\overrightarrow{w}$, and we must additionally compute $\frac{\partial L}{\partial \beta_0}$.
 # $\color{red}{\text{TODO: intercept vector issue}}$
-# The forward pass simply calculated the loss via a series of nested functions, and the backward pass will now compute the partial derivatives of each function, evaluating those derivatives at the functions' inputs and multiplying them together. 
-# 
-# 
+# The forward pass simply calculated the loss via a series of nested functions, and the backward pass will now compute the partial derivatives of each function, evaluating those derivatives at the functions' inputs and multiplying them together.
+#
+#
 # ## Backward Pass for Linear Regression
-# In the backward pass, we compute the derivative of each constituent function and evaluate those derivatives at the inputs that those functions received on the forward pass, and then multiply all these derivatives together as the chain rule dictates. 
-# 
-# The derivative product we must compute is: 
-# $$ 
+# In the backward pass, we compute the derivative of each constituent function and evaluate those derivatives at the inputs that those functions received on the forward pass, and then multiply all these derivatives together as the chain rule dictates.
+#
+# The derivative product we must compute is:
+# $$
 # \begin{aligned}
-# \frac{\partial L}{\partial \overrightarrow{w}} 
+# \frac{\partial L}{\partial \overrightarrow{w}}
 #   &= \frac{\partial }{\partial P}\Lambda(P, Y) \times \frac{\partial }{\partial N} \alpha(N, B)  \times \frac{\partial }{\partial \overrightarrow{w}} \nu(X, \overrightarrow{w}) \\
 #   &= \frac{\partial }{\partial \overrightarrow{P}} \Lambda(\overrightarrow{P}, \overrightarrow{Y}) \times \frac{\partial}{\partial \overrightarrow{N}} \alpha(\overrightarrow{N}, \overrightarrow{\beta_0}) \times \frac{\partial }{\partial \overrightarrow{w}} \nu(X, \overrightarrow{w}) \\
-#   &= \frac{\partial \Lambda}{\partial \overrightarrow{P}} \times \frac{\partial \alpha}{\partial \overrightarrow{N}} \times \frac{\partial \nu}{\partial \overrightarrow{w}} 
+#   &= \frac{\partial \Lambda}{\partial \overrightarrow{P}} \times \frac{\partial \alpha}{\partial \overrightarrow{N}} \times \frac{\partial \nu}{\partial \overrightarrow{w}}
 # \end{aligned}
 # $$
-# 
-# 
-# ### Calculating Loss Gradient $\frac{\partial \Lambda}{\partial \overrightarrow{w}}$: 
-# 
+#
+#
+# ### Calculating Loss Gradient $\frac{\partial \Lambda}{\partial \overrightarrow{w}}$:
+#
 # **The Direct Way**
-# 
+#
 # $$
 # \frac{\partial \Lambda}{\partial \overrightarrow{w}} = \begin{pmatrix}
 #   w_1 \\ w_2 \\ \vdots \\ w_k
 # \end{pmatrix}
 # $$
-# 
-# 
+#
+#
 # **The Chain Rule Way**
-# 
+#
 # 1. **Step 1:** Compute $\frac{\partial \Lambda}{\partial \overrightarrow{P}}$
-# 
+#
 # $$
 # \begin{aligned}
-# \frac{\partial }{\partial \overrightarrow{P}} \Lambda(\overrightarrow{P}, \overrightarrow{Y}) 
+# \frac{\partial }{\partial \overrightarrow{P}} \Lambda(\overrightarrow{P}, \overrightarrow{Y})
 #   &= \frac{\partial}{\partial \overrightarrow{P}} \Bigg( \frac{(\overrightarrow{Y} - \overrightarrow{P})^2} {b} \Bigg) \\
 #   &= \frac{-1 \cdot (2 \cdot (\overrightarrow{Y} - \overrightarrow{P})) } {b}
 # \end{aligned}
@@ -494,72 +494,83 @@ def forwardLinearRegression(X_batch: Tensor, y_batch: Tensor, weights: Dict[str,
 
 
 # %% [markdown]
-# Preliminary variable Set-up: 
+# Preliminary variable Set-up:
 # %%
-b, k, one = 5, 8, 1
+# NOTE: using numbers to construct actual matrices
+b_, k_, l_ = 5, 8, 1
+# NOTE: using the symbolic shapes so that matrix derivative of MatrixSymbols Xm * wm can occur correctly
+b, k, l = symbols('b k l', commutative=True)
 
 # Variables
-X = Matrix(b, k, lambda i,j : var_ij('x', i, j))
-Xm = MatrixSymbol('X', X.shape[0], X.shape[1]) 
+Xelem = Matrix(b_, k_, lambda i,j : var_ij('x', i, j))
+X = MatrixSymbol('X', b, k)
 Xs = Symbol('X', commutative=True)
 
-w = Matrix(k, one, lambda i,j : var_i('w', i))
-wm = MatrixSymbol('\overrightarrow{w}', w.shape[0], w.shape[1]) #,is_commutative=True)
+welem = Matrix(k_, l_, lambda i,_ : var_i('w', i))
+w = MatrixSymbol('\overrightarrow{w}', k, l)
 ws = Symbol('\overrightarrow{w}', commutative=True)
 
 # matrix variable for sympy Lambda function arguments
-Mm = MatrixSymbol('M', i, j) #, is_commutative=True)# abstract shape
+M = MatrixSymbol('M', i, j) #, is_commutative=True)# abstract shape
 
 
 # N = X * w
 v = Function("nu",applyfunc=True)
 v_ = lambda a,b: a*b
 #vL = Lambda((a,b), a*b)
-VL = Lambda((Xm,wm), MatrixSymbol('V', Xm.shape[0], wm.shape[1]))
-vN = lambda mat1, mat2: Matrix(mat1.shape[0], mat2.shape[1], lambda i, j: Symbol("n_{}{}".format(i+1, j+1)), is_commutative=True)
+VL = Lambda((X,w), MatrixSymbol('V', b, l) ) #Xm.shape[0], wm.shape[1]))
+vN = lambda mat1, mat2: Matrix(mat1.shape[0], mat2.shape[1], lambda i,j: var_ij('n', i, j))
 
-Nm = MatrixSymbol('\overrightarrow{N}', b, one) #, is_commutative=True)
+N = MatrixSymbol('\overrightarrow{N}', b, l) #, is_commutative=True)
 Ns = Symbol('\overrightarrow{N}', commutative=True)
-Nelem = vN(X, w)
-Nspec = v_(X, w)
-N = v(Xm,wm)
+Nelem = vN(Xelem, welem)
+Nspec = v_(Xelem, welem)
+Nfunc = v(X,w)
 
 
 # Alpha = N + B = X*w + beta_0
+B = MatrixSymbol('\overrightarrow{\\beta_0}', b , l)
 Bs = Symbol('beta_0', is_commutative=True)
-Belem = Matrix(b, 1, lambda i, j : Bs)
-Bm = MatrixSymbol('\overrightarrow{\\beta_0}', b , one)
+Belem = Matrix(b_, l_, lambda i, j : Bs)
+# TODO when N is symbol use right arrow overtop? For B too or not? Be consistent.
 
-alpha = Function('alpha')
-alpha_ = lambda N, B: N + B 
+sigma = Function('sigma', commutative=True)
+sigma_ = lambda matrix : matrix.applyfunc(sigma)
+
+
+alpha = Function('alpha', applyfunc=True, commutative=True)
+alpha_ = lambda N_, B_: N_ + B_ # take in matrix symbols to add them.
+# Function to avoid errors with replacing in alpha two-arg function - just subtitute the desired matrix result right in alpha (problem is cannot do this straight from alpha or Pfunc because alpha has two arguments and for some reason error is thrown ...)
+alphaSub = lambda X, w, B: alpha(sigma(v(X, w)).replace(v, v_).replace(sigma, sigma_).diff(w), B)
+#alphaL = Lambda( (N, B), N + B)
+#alphaApply = Lambda( (N, B), alpha(N, B))
 #alphaL = Lambda( ())
 
-P = alpha(N, Bm)
+Pfunc = alpha(Nfunc, B)
+P = MatrixSymbol('\overrightarrow{P}', b, l)
 Ps = Symbol('\overrightarrow{P}', commutative=True)
-Pm = MatrixSymbol('\overrightarrow{P}', b, one) #, is_commutative=True)
-Pelem = Matrix(b, 1, lambda i, _: var_i('p', i), is_commutative=True)
-Pspec = P.subs({Xm: X, wm:w}).replace(v, v_).subs(Bs, Belem).replace(alpha, alpha_)
+Pelem = Matrix(b_, l_, lambda i, _: var_i('p', i))
+Pspec = Pfunc.subs({X: Xelem, w:welem}).replace(v, v_).replace(B, Belem).replace(alpha, alpha_)
 #alpha_(Nspec, B)
 
 
 # Lambda function: L = Lambda(P, Y)
+Y = MatrixSymbol('\overrightarrow{Y}', b, l)
 Ys = Symbol('\overrightarrow{Y}', commutative=True)
-Ym = MatrixSymbol('\overrightarrow{Y}', b, one) #, is_commutative=True)
-Yelem = Matrix(b, one, lambda i, _: var_i('y', i), is_commutative=True)
+Yelem = Matrix(b_, l_, lambda i, _: var_i('y', i))
 
-lambd = Function("lambda")
-bs = Symbol('b', is_commutative=True)
-lambd_ = lambda P, Y : (Y - P)**2 / bs
+lambd = Function("lambda", commutative=True)
+lambd_ = lambda P_, Y_ : (Y_ - P_)**2 / b #Takes matrix symbols to calculate MSE
 
 #L = lambd(alpha(v(Xs, ws), B), Ys)
 #Lfunc = lambda X_mat, w_vec, B, Y_vec: lambd(alpha(v(X_mat, w_vec), B), Y_vec)
 #L = Lfunc(Xm, wm, B, Ym)
-Lm = lambd(alpha(v(Xm, wm), Bm), Ym)
+L = lambd(alpha(v(X, w), B), Y)
 # TODO: need this symbol, non-matrix version because otherwise replacing and derivating results in error" noncommutative scalars in matmul are not supported (even if ANY one of the arguments here are Matrix or MatrixSymbol)
 # TODO so now I don't know whether the following derivations with the SYMBOLS instead of MATRIX SYMBOL forms (as in ch1) are actually correct (meaning I don't think the correct matrix calculus rules are being applied)
 Ls = lambd(alpha(v(Xs, ws), Bs), Ys)
 
-showGroup([Lm, Ls])
+showGroup([L, Ls])
 
 
 # %% codecell
@@ -574,32 +585,71 @@ showGroup([
 
 # %%
 # dL_dw_abstract = # TODO how to get the matrices rules to apply like in Ch1?
+# NOTE: interesting here sympy knew how to apply the product rule (WHY, is it just because there is only one kind of argument, the X? Does it not work when two kinds like A and B?)
+
+
+# %%
+sigma(v(X, X.T)).replace(v, v_).replace(sigma, sigma_).diff(X)
+
+# %%
+A = MatrixSymbol('A', k, a)
 showGroup([
-    Ls.replace(v, v_).diff(ws), 
+    sigma(v(X, A)).replace(v, v_).replace(sigma, sigma_).diff(X),
+    sigma(v(X, A)).replace(v, v_).replace(sigma, sigma_).diff(A)
+])
+
+# %%
+showGroup([
+    sigma(v(X, w)).replace(v, v_).replace(sigma, sigma_).diff(X),
+    sigma(v(X, w)).replace(v, v_).replace(sigma, sigma_).diff(w)
+])
+# %%
+showGroup([
+    Ls.replace(v, v_).diff(ws),
     Ls.replace(v, v_).diff(ws).replace(alpha(Xs*ws, Bs), Ps)
 ])
+
+# %%
+#alpha(sigma(v(X, w)), Bs).replace(v, v_).replace(sigma, sigma_).diff(w)
+# %%
+#alpha(*[sigma(v(X,w)), Bs])
+alphaSub(X, w, B)
+# NTOTE: below is not what I want, need the X and w to be both  matrix symbols so the transpose happens (correct matrix calc)
+# alpha(*[sigma(v(X,w)), Bs]).replace(v, v_).replace(sigma, sigma_).replace(w, ws).diff(ws)
+# %%
+_d = Symbol('d', commutative=True)
+Lambda(_d, sigma(_d).diff(_d))
+
+# %%
+# TODO left off here trying to find a way to replace the sigma composition to result just in the X^T derivative ...
+matdiff = sigma(v(X, w)).replace(v, v_).replace(sigma, sigma_).diff(w)
+matdiff.replace(X*w, Identity(b))
+
+
+
+
 # %% codecell
-# Chain rule component way: 
+# Chain rule component way:
 dL_dP = Ls.diff(alpha(v(Xs, ws), Bs))
 # Ls.replace(alpha(v(Xs, ws), Bs), Ps).diff(Ps)
 
 showGroup([
-    dL_dP, 
+    dL_dP,
     dL_dP.replace( alpha(v(Xs, ws), Bs), Ps),
     dL_dP.replace( alpha(v(Xs, ws), Bs), Ps).replace(lambd, lambd_),
     dL_dP.replace( alpha(v(Xs, ws), Bs), Ps).replace(lambd, lambd_).doit()
 ])
 # %% codecell
-da_dN = alpha(v(Xs, ws), Bs).diff(v(Xs, ws))
+dP_dN = alpha(v(Xs, ws), Bs).diff(v(Xs, ws))
 
 showGroup([
-    da_dN, 
-    da_dN.replace(v(Xs, ws), Ns), 
-    da_dN.replace(v(Xs, ws), Ns).replace(alpha(Ns, Bs), Ps),
-    da_dN.replace(v(Xs, ws), Ns).replace(alpha, alpha_).replace(Bs, Bm).replace(Ns, Nm),
-    da_dN.replace(v(Xs, ws), Ns).replace(alpha, alpha_).replace(Bs, Bm).replace(Ns, Nm).doit()
+    dP_dN,
+    dP_dN.replace(v(Xs, ws), Ns),
+    dP_dN.replace(v(Xs, ws), Ns).replace(alpha(Ns, Bs), Ps),
+    dP_dN.replace(v(Xs, ws), Ns).replace(alpha, alpha_).replace(Bs, B).replace(Ns, N),
+    dP_dN.replace(v(Xs, ws), Ns).replace(alpha, alpha_).replace(Bs, B).replace(Ns, N).doit()
 ])
-# TODO left off here need to find a way to make this a matrix of ones not a tensor of tensors. 
+# TODO left off here need to find a way to make this a matrix of ones not a tensor of tensors.
 from sympy import tensorcontraction
 ex = alpha(Nelem, Belem).replace(alpha, alpha_).diff(Nelem)
 #ex.shape
@@ -610,17 +660,18 @@ contr2 = tensorcontraction(contr, (0,))
 
 
 ### TODO URGENT HERE
-# TODO try the old way matrix deriv then do the tensor contraction to see if the result is the same with multiplying by parts manually and substituting the matrix there. 
+# TODO try the old way matrix deriv then do the tensor contraction to see if the result is the same with multiplying by parts manually and substituting the matrix there.
 # %% codecell
-dN_dw = v(Xs, ws).sub({Xs:Xm, ws:wm}).replace(v, v_).diff(wm)
+dN_dw = v(Xs, ws).subs({Xs:X, ws:w}).replace(v, v_).diff(w)
 
 dN_dw
 # %%
-# Putting them together: 
-dL_dP * da_dN * dN_dw
+# Putting them together:
+dL_dP * dP_dN * dN_dw
 # %%
 # TODO left off here
 # TODO testing how the matrix multiplication occurs -- not sure this product here is correct
-test = Xs * dL_dP * da_dN
+test = Xs * dL_dP * dP_dN
 test
-test.replace(Xs, X).doit().replace(X, Xs).replace(v(Xs, ws), Ns).replace(alpha(Ns, Bs), Ps)
+#test.replace(Xs, X).doit().replace(X, Xs).replace(v(Xs, ws), Ns).replace(alpha(Ns, Bs), Ps)
+# %% codecell
